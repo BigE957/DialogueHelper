48977606951a07319d12d51746632a2e39f68b08
Big E <evanmorrison42@gmail.com>
2024-12-13 21:34:26 -0500

    + Updated Emoticon's base Update behavior

--- UI/Dialogue/Emoticons/Emoticon.cs
+++ UI/Dialogue/Emoticons/Emoticon.cs
@@ -2,35 +2,31 @@
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.UI;
 
-namespace DialogueHelper.UI.Dialogue.Emoticons
+namespace DialogueHelper.UI.Dialogue.Emoticons;
+
+public class BaseEmoticon : UIElement
 {
-    public class Emoticon : UIElement
+    public int Counter = 0;
+    public int FrameNum = 0;
+    public float ImageScale = 1f;
+    public float Opacity = 1f;
+    internal bool Fading = false;
+    public float Rotation;
+    public Color Color = Color.White;
+    public SpriteEffects spriteEffects;
+    internal Rectangle SpeakerHeadArea;
+
+    public override void Update(GameTime gameTime)
     {
-        public int Counter = 0;
-        public int FrameNum = 0;
-        public float ImageScale = 1f;
-        public float Opacity = 1f;
-        internal bool Fading = false;
-        public float Rotation;
-        public Color Color = Color.White;
-        public SpriteEffects spriteEffects;
-        internal Rectangle SpeakerHeadArea;
+        base.Update(gameTime);
 
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-
-            if (Fading)
-            {
-                if(--Counter < 0)
-                    Remove();
-            }
-            else
-                Counter++;
-        }
-
-        public virtual int TimeToAppear => 0;
-
-        public virtual Vector2 OffsetPosition() => Vector2.Zero;
+        if (Fading)
+            Counter--;
+        else
+            Counter++;
     }
+
+    public virtual int TimeToAppear => 0;
+
+    public virtual Vector2 OffsetPosition() => Vector2.Zero;
 }
