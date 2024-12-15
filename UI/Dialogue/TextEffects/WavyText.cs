99fbc5ab6eee9fa795c9e79994a864c28ac4b990
Big E <evanmorrison42@gmail.com>
2024-12-15 11:56:32 -0500

    + Fixed Using Errors

--- UI/Dialogue/TextEffects/WavyText.cs
+++ UI/Dialogue/TextEffects/WavyText.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
 
 namespace DialogueHelper.UI.Dialogue.TextEffects;
 public class WavyText : TextEffect
